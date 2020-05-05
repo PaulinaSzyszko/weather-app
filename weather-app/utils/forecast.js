b0e502055c3b9223d07e33bcb2350958c8b21564
Paulina Szyszko <paulinabuczel@gmail.com>
2020-05-05 21:51:29 +0200

    Change http source address

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) =>{
     
-    const url= 'http://api.weatherstack.com/current?access_key=db1a12574eca7204fb478657cfbced6b&query='+latitude + ',' + longitude +'&units=f';
+    const url= 'http://api.weatherstack.com/current?access_key=db1a12574eca7204fb478657cfbced6b&query='+latitude + ',' + longitude +'&units=m';
     request({url, json: true}, (error, {body}) => {
          if (error){
              callback ('Unable to connect to location services!', undefined)
