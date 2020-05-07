2979ceb70cc9edab8e8d5bff522b2a98cfa34d33
Paulina Szyszko <paulinabuczel@gmail.com>
2020-05-07 18:23:40 +0200

    New forecast information added to the main page

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) =>{
-    
+    //console.log(latitude,longitude)
     const url= 'http://api.weatherstack.com/current?access_key=db1a12574eca7204fb478657cfbced6b&query='+latitude + ',' + longitude +'&units=m';
     request({url, json: true}, (error, {body}) => {
          if (error){
@@ -9,7 +9,10 @@ const forecast = (latitude, longitude, callback) =>{
          } else if (body.error){
              callback ('Incorrect latitude or longitude', undefined)
          } else {
-             callback (undefined , body.current.weather_descriptions[0] +'. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.' )
+             callback (undefined , body.current.weather_descriptions[0] +
+                 '. It is currently ' + body.current.temperature + ' degress out. ' +
+                 'It feels like ' + body.current.feelslike + ' degress out. ' + 'The wind speed is ' + body.current.wind_speed + ' ' +
+             'The pressure is ' + body.current.pressure)
          }
     }) 
 }
